45e31deab4c1000dd771973c80f5161e45290529
darnell-love27 <101828196+darnell-love27@users.noreply.github.com>
2023-05-10 14:23:32 -0400

    Add files via upload

---
+++ Image_Filters/__init__.py
@@ -0,0 +1,16 @@
+from Filter_Choices.filter_choices import Filters, valstrax.png
+
+__author__ = 'Miles Adedjouma, Jamar Bailey III, Darnell Love'
+__email__ = 'darnell.love@bison.howard.edu'
+__version__ = '0.1.0'
+
+# module level doc-string
+__doc__ = """
+Filter Choices
+================
+
+Description
+-----------
+The project was meant to be a test of the package functions in Github. In order to so, we created a project that was a photo filter function package using Python. The goal was to allow the user to take the package in order to add it to a greater function or to modify the function to allow users to add their own filters to the image. Furthermore, the function would allow users to input their own images in the functions. 
+
+"""
